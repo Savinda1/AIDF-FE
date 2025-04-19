daef6bb7944064232c57adbe132649a1b5f226bd
Amila <155749022+Savinda1@users.noreply.github.com>
2025-04-19 10:53:34 +0530

    url update

--- src/lib/api/api.js
+++ src/lib/api/api.js
@@ -7,7 +7,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
- // baseQuery: fetchBaseQuery({ baseUrl: `${BACKEND_URL}/api/`,
+  //baseQuery: fetchBaseQuery({ baseUrl: `${BACKEND_URL}/api/`,
  baseQuery: fetchBaseQuery({ baseUrl: "https://aidf-horizone-backend-amila.vercel.app/api/",
 
     prepareHeaders: async (headers, { getState }) => {
